4e10f9332a9e3e75026e16b4cdd6333633d58bb3
Ssunk <ssunkkan@gmail.com>
2024-12-19 22:41:11 +0800

    添加接口访问频率限制

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 app = FastAPI()
 
+
 templates = Jinja2Templates(directory="templates")
+
+# 创建 limiter 实例，使用 `get_remote_address` 获取客户端 IP 地址
+limiter = Limiter(key_func=get_remote_address)
+
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -23,12 +30,13 @@ async def read_item(request: Request):
 
 
 @app.get("/video/share/url/parse")
-async def share_url_parse(url: str):
+@limiter.limit("2/minute")  # 限制每分钟最多访问 2 次
+async def share_url_parse(request: Request,url: str):
     url_reg = re.compile(r"http[s]?:\/\/[\w.-]+[\w\/-]*[\w.-]*\??[\w=&:\-\+\%]*[/]*")
     video_share_url = url_reg.search(url).group()
-
     try:
         video_info = await parse_video_share_url(video_share_url)
+        print(video_info.__dict__)
         return {"code": 200, "msg": "解析成功", "data": video_info.__dict__}
     except Exception as err:
         return {
@@ -38,7 +46,8 @@ async def share_url_parse(url: str):
 
 
 @app.get("/video/id/parse")
-async def video_id_parse(source: VideoSource, video_id: str):
+@limiter.limit("2/minute")  # 限制每分钟最多访问 2 次
+async def video_id_parse(request: Request,source: VideoSource, video_id: str):
     try:
         video_info = await parse_video_id(source, video_id)
         return {"code": 200, "msg": "解析成功", "data": video_info.__dict__}
